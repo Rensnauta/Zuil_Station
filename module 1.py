f486f9013a138f721ef0639aee34a5e40ffba986
rensn <rens.nauta@student.hu.nl>
2023-10-02 09:20:40 +0200

    Module 1 versie 1

--- module 1.py
+++ module 1.py
@@ -4,7 +4,7 @@ import random
 
 stations = ['Heerenveen', 'Utrecht-Centraal', 'Driebergen-Zeist']
 
-with open("zuil.csv", 'w', newline='') as f:
+with open("berichten.csv", 'w', newline='') as f:
     csv_writer = csv.writer(f)
     csv_writer.writerow(["Naam", "Datum & Tijd", "Station", "Bericht"])
 
@@ -13,6 +13,10 @@ def input_naam():  # creeër een functie om de naam van een bezoeker te vragen
     naam = input("Wat is uw naam?")
     if len(naam) < 51:  # bepaal of de ingevoerde naam niet te lang is
         return naam  # sla de naam op als variabele
+    elif len(naam) == 0:
+        print('Naam is anoniem')
+        naam = 'anoniem'
+        return naam
     else:  # als de naam die de bezoeker heeft ingevuld te lang is
         print("Uw naam is te lang")
         input_naam()  # herhaal de functie, vraag om een nieuwe input
@@ -28,9 +32,9 @@ def input_bericht():  # creeër een functie om het bericht van een bezoeker te k
         input_bericht()
 
 def output_csv():
-    with open("zuil.csv", "a", newline='') as f:
+    with open("berichten.csv", "a", newline='') as f:
         csv_writer = csv.writer(f)
-        csv_writer.writerow([naam, bericht, time.strftime('%H:%M:%S, %d %b %y'), stations[random.randint(0, 2)]])
+        csv_writer.writerow([naam, time.strftime('%H:%M:%S, %d %b %y'), stations[random.randint(0, 2)], bericht])
 
 
 while True:
