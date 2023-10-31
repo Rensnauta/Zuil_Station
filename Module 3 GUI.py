e733925b5eb2c6049f64b2d2165e58f6e311f5b5
rensn <rens.nauta@student.hu.nl>
2023-10-31 13:45:44 +0100

    programma werkt ook wanneer minder dan 5 berichten zijn opgeslagen

--- Module 3 GUI.py
+++ Module 3 GUI.py
@@ -1,47 +1,36 @@
 import psycopg2
 
 berichten = []
+lst = []
 connection = "host='20.254.33.20' dbname='stationszuil' user='postgres' password='Welkom01!'"
 
 def database_retrieve():
-    berichten.clear()
     conn = psycopg2.connect(connection)
     cursor = conn.cursor()
-    cursor.execute("""SELECT bericht FROM moderatie WHERE oordeel = 'goedgekeurd' order by berichtid desc limit 5""")
+    cursor.execute("""SELECT gebruikersn, datum, bericht FROM moderatie WHERE oordeel = 'goedgekeurd' order by berichtid desc limit 5""")
     data = cursor.fetchall()
     conn.close()
     for i in data:
-        berichten.append(i[0])
+        naam = i[0]
+        datum = i[1]
+        bericht = i[2]
+        opslag = '\n' + naam + ': ' + bericht + ' \n ' + datum + '\n'
+        lst.append(opslag)
+    print(lst)
+
+database_retrieve()
+
+
+from tkinter import *
+root = Tk()
+for i in range(len(lst)):
+    label = Label(master=root,
+    text=lst[i],
+    background='#FFCC18')
+    label.pack()
+
+root.mainloop()
 
 
 database_retrieve()
 
-from tkinter import *
-root = Tk()
-
-label = Label(master=root,
-text=berichten[0],
-background='yellow')
-label.pack()
-
-label2 = Label(master=root,
-text=berichten[1],
-background='yellow')
-label2.pack()
-
-label3 = Label(master=root,
-text=berichten[2],
-background='yellow')
-label3.pack()
-
-label4 = Label(master=root,
-text=berichten[3],
-background='yellow')
-label4.pack()
-
-label5 = Label(master=root,
-text=berichten[4],
-background='yellow')
-label5.pack()
-
-root.mainloop()
