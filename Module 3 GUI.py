690d022a6d3c76207d947b03f0e712bc75d91d9f
rensn <rens.nauta@student.hu.nl>
2023-11-02 13:25:01 +0100

    keuze beperkt tot 3 stations

--- Module 3 GUI.py
+++ Module 3 GUI.py
@@ -8,34 +8,41 @@ berichten = []
 lst = []
 connection = "host='20.254.33.20' dbname='stationszuil' user='postgres' password='Welkom01!'"
 
+
 def database_retrieve():
     conn = psycopg2.connect(connection)
     cursor = conn.cursor()
-    cursor.execute("""SELECT gebruikersn, datum, bericht FROM moderatie WHERE oordeel = 'goedgekeurd' order by berichtid desc limit 5""")
+    cursor.execute(
+        """SELECT gebruikersn, datum, bericht FROM moderatie WHERE oordeel = 'goedgekeurd' order by berichtid desc limit 5""")
     data = cursor.fetchall()
     conn.close()
     for i in data:
         naam = i[0]
         datum = i[1]
         bericht = i[2]
-        opslag = '\n' + naam + ': ' + bericht + ' \n ' + datum + '\n'
+        opslag = '\n' + naam + ': ' + bericht + ' \n ' + datum
         lst.append(opslag)
-    print(lst)
+
+
 
 database_retrieve()
 
-
 from tkinter import *
+import tkinter.font
 root = Tk()
 root.configure(bg='#FFCC18')
+root.geometry("600x800")
 for i in range(len(lst)):
     label = Label(master=root,
-    text=lst[i],
-    background='#FFCC18')
-    label.pack()
+                  text=lst[i],
+                  background='#FFCC18',
+                  font=('Helvetica', 14))
+    label.pack(side='bottom', fill='x')
+
+image = PhotoImage(file="C:/Users/rensn/Downloads/NS.png")
+label = Label(root, image=image)
+label.place(x=0, y=0)
 
 root.mainloop()
 
-
 database_retrieve()
-
