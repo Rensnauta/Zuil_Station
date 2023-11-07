0355c82996ff57692e34e363b8ef693deaab1821
rensn <rens.nauta@student.hu.nl>
2023-11-07 12:42:53 +0100

    database opgedeeld in 3 tabellen

--- Moderatie.py
+++ Moderatie.py
@@ -5,9 +5,38 @@ import time
 connection = "host='20.254.33.20' dbname='stationszuil' user='postgres' password='Welkom01!'"
 lst = []
 
+def database_upload(data):
+    conn = psycopg2.connect(connection)  # verbind met de database via de connection variabele
+    cursor = conn.cursor()
+    query = """INSERT INTO bericht(naam, datum_bericht, tijd_bericht, station_fk, bericht, oordeel, 
+    tijd_oordeel, datum_oordeel, moderator_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    cursor.execute(query, data)
+    conn.commit()
+    conn.close()
+
+
+def moderatie():
+    with open("berichten.csv", 'r', newline='') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            print(row[4])
+            oordeel = input('Keurt u dit bericht goed? y/n:')
+            if oordeel == 'y':
+                row += ['goedgekeurd', time.strftime('%H:%M:%S'), time.strftime('%d %b %y'), mod_id]
+                database_upload(row)
+            elif oordeel == 'n':
+                row += ['afgekeurd', time.strftime('%H:%M:%S'), time.strftime('%d %b %y'), mod_id]
+                database_upload(row)
+            else:
+                print('Er is iets misgegaan')
+                moderatie()
+
+
 def inloggen():
-    email = input('Email:')
-    wachtwoord = input('Wachtwoord:')
+    # email = input('Email:')
+    # wachtwoord = input('Wachtwoord:')
+    email = 'voorbeeld123@email.com'
+    wachtwoord = 'Welkom01!'
     try:
         conn = psycopg2.connect(connection)  # verbind met de database via de connection variabele
         cursor = conn.cursor()
@@ -20,38 +49,17 @@ def inloggen():
             break
         else:
             raise ValueError()
-        return id
+        return id[0][0]
     except ValueError:
         print('Er is iets misgegaan')
         inloggen()
 
 
+
 mod_id = inloggen()
-
-
-def moderatie():
-    with open("berichten.csv", 'r', newline='') as file:
-        csv_reader = csv.reader(file)
-        for row in csv_reader:
-            print(row[4])
-            oordeel = input('Keurt u dit bericht goed? y/n:')
-            if oordeel == 'y':
-                row += ['goedgekeurd', time.strftime('%H:%M:%S'), time.strftime('%d %b %y')]
-                database_upload(row)
-            elif oordeel == 'n':
-                row += ['afgekeurd', time.strftime('%H:%M:%S'), time.strftime('%d %b %y')]
-                database_upload(row)
-            else:
-                print('Er is iets misgegaan')
-                moderatie()
-
-def database_upload(data):
-    conn = psycopg2.connect(connection)  # verbind met de database via de connection variabele
-    cursor = conn.cursor()
-    query = """INSERT INTO moderatie (naam, bericht, tijd, station, bericht, oordeel, tijdoordeel, 
-    datumoordeel,) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
-    cursor.execute(query, data)
-    conn.commit()
-    conn.close()
-
 moderatie()
+
+
+
+
+
